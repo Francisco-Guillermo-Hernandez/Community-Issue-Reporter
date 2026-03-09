f09913d37406a91fcdbf9f258faf21aa54483736
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-03-08 23:19:37 -0600

    ft: Add a new model

--- Community Issue Reporter/Model/Petition.swift
+++ Community Issue Reporter/Model/Petition.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct Petition: Identifiable, Codable {
+    let id: UUID
+    let title: String
+    let description: String
+    let targetSignatures: Int
+    let currentSignatures: Int
+    let categoryId: Int
+    let statusId: Int
+    let reportedBy: UUID
+    let disabled: Bool
+    let createdAt: Date
+    let updatedAt: Date
+}

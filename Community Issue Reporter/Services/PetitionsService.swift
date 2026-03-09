ed120e5c6c67b999b6cbadf1b2bcf74d1197e69e
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-03-08 23:21:49 -0600

    ft: Adding Petition Service

--- Community Issue Reporter/Services/PetitionsService.swift
+++ Community Issue Reporter/Services/PetitionsService.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct PetitionsService {
+    private let client: ServiceClient
+    
+    init(client: ServiceClient = ServiceClient()) {
+        self.client = client
+    }
+    
+    func fetchPetitions() async throws -> [Petition] {
+        return try await self.client.get(path: "petitions")
+    }
+    
+    func fetchPetition(id: Int) async throws -> Petition {
+        return try await self.client.get(path: "petitions/\(id)")
+    }
+    
+}

05f04565e213141b4b48f9385ffc4b4526691cbb
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-03-11 23:51:05 -0600

    ft: Working on a custom Alert

--- Community Issue Reporter/Views/Components/CustomAlert.swift
+++ Community Issue Reporter/Views/Components/CustomAlert.swift
@@ -7,12 +7,40 @@
 
 import SwiftUI
 
-struct CustomAlert: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+@ViewBuilder
+func CustomAlert(message: String, type: AlertType = .success) -> some View {
+    ZStack {
+        HStack(spacing: 16) {
+            Image(systemName: type.icon)
+            Text(message)
+                .font(.caption)
+                .foregroundStyle(.secondary)
+                .lineLimit(1)
+                .truncationMode(.tail)
+        }
+        .padding(.horizontal, 12)
+        .frame(minWidth: 100, maxWidth: 250)
+        .frame(height: 40)
+        .background(
+            RoundedRectangle(cornerRadius: 8)
+                .fill(.ultraThinMaterial)
+        )
     }
 }
 
-#Preview {
-    CustomAlert()
+enum AlertType: String, CaseIterable, Identifiable {
+    case success =  "checkmark.circle.fill"
+    case notice = "exclamationmark.triangle.fill"
+    case error =  "xmark.octagon.fill"
+    
+    var id: String { self.rawValue }
+    var icon: String { self.rawValue }
+}
+
+
+struct CustomizedTransition: Transition {
+    func body(content: Content, phase: TransitionPhase) -> some View {
+        content
+            .scaleEffect(phase.isIdentity ? 1 : 0)
+    }
 }

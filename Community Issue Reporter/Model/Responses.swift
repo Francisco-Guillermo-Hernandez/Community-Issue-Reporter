11f2ac8cbcd15235499654ebebe19ab7f25a0b4b
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-03-11 23:49:56 -0600

    ft: Add a generic response

---
+++ Community Issue Reporter/Model/Responses.swift
@@ -0,0 +1,13 @@
+//
+//  Responses.swift
+//  Community Issue Reporter
+//
+//  Created by Francisco Hernandez on 9/3/26.
+//
+
+import Foundation
+
+
+struct GenericResponse: Identifiable, Codable {
+   var id: String
+}
---
+++ Community Issue Reporter/Views/Components/CustomAlert.swift
@@ -0,0 +1,18 @@
+//
+//  CustomAlert.swift
+//  Community Issue Reporter
+//
+//  Created by Francisco Hernandez on 10/3/26.
+//
+
+import SwiftUI
+
+struct CustomAlert: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomAlert()
+}

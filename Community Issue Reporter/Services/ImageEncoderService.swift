53807a0eb6564ed982a9f5208e52e5df4d93f198
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-03-25 22:32:00 -0600

    ft: Add new service to encode images into webP

--- Community Issue Reporter/Services/ImageEncoderService.swift
+++ Community Issue Reporter/Services/ImageEncoderService.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import WebP
+import UIKit
+
+struct ImageEncoderService {
+
+    
+    func prepareToSent(reportId: String, images: [UIImage], completion: @escaping (String) -> ()) {
+        Task.detached(priority: .background) {
+            do {
+                var webPData: [Data] = []
+                
+                for image in images {
+                    let encodedData = try autoreleasepool {
+                        return try WebPEncoder().encode(image, config: .preset(.picture, quality: 80))
+                    }
+                    print("Processing image")
+                    
+                    webPData.append(encodedData)
+                }
+                
+                _ = try await ReportsService().attachPicture(reportId: reportId, imagesData: webPData)
+                
+                await MainActor.run {
+                    completion("completion")
+                }
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+}

fb15cff2b24144bf83383e533652627aa1174a58
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-03-25 22:29:28 -0600

    ft: Add new helpers

--- Community Issue Reporter/Helpers/DeepLinkHandler.swift
+++ Community Issue Reporter/Helpers/DeepLinkHandler.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+func deepLinkHandler(_ url: URL) {
+    let components = URLComponents(url: url, resolvingAgainstBaseURL: true)
+    let path = url.path
+    
+    guard let petitionId = components?.queryItems?.first(where: {
+        $0.name == "id"
+    })?.value else { return }
+    
+    print("path: \(path)")
+    print("petition: \(petitionId)")
+    
+}
--- Community Issue Reporter/Helpers/WithTimeout.swift
+++ Community Issue Reporter/Helpers/WithTimeout.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+enum TimeoutError: Error {
+    case timedOut
+}
+
+func withTimeout<T: Sendable>(after duration: Duration, operation: @escaping @Sendable () async throws -> T ) async throws -> T {
+    try await withThrowingTaskGroup(of: T.self) { group in
+        
+        group.addTask {
+            return try await operation()
+        }
+        
+
+        group.addTask {
+            try await Task.sleep(for: duration)
+           
+            try Task.checkCancellation()
+            throw TimeoutError.timedOut
+        }
+        
+
+        guard let value = try await group.next() else {
+            throw TimeoutError.timedOut
+        }
+        
+        group.cancelAll()
+        return value
+    }
+}

8edaa160b71bbf23d54c78a74d4de0b64968d007
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-03-25 22:25:54 -0600

    ft: Add a custom alert

--- Community Issue Reporter/TabBarView.swift
+++ Community Issue Reporter/TabBarView.swift
@@ -9,20 +9,24 @@ import SwiftUI
 
 
 struct TabBarView: View {
+    @Environment(\.colorScheme) var colorScheme
     
     @State private var searchText: String = ""
     @State private var selectedTab: Int = 1
     @State private var oldSelectedTab: Int = 1
     @State private var isPresented: Bool = false
+    @State private var message: String = ""
+    @State private var type: AlertType = .success
+    @State private var show = false
     
     var body: some View {
         TabView(selection: $selectedTab) {
             
-            Tab("Reports", systemImage: "map", value: 1) {
+            Tab("Issues", systemImage: "map", value: 1) {
                 ReportsView()
             }
             
-            Tab("Sign petitions", systemImage: "list.bullet.rectangle", value: 2) {
+            Tab("Sign petitions", systemImage: "signature", value: 2) {
                SignRequestsView()
             }
             
@@ -38,7 +42,28 @@ struct TabBarView: View {
             }
         }
         .sheet(isPresented: $isPresented) {
-            ReportView()
+            ReportView(onCompletion: { incomingMessage, alertType in
+                message = incomingMessage
+                type = alertType
+                show = true
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 6.0) {
+                    self.show = false
+                    self.isPresented = false
+                }
+            })
+        }
+        .overlay(alignment: .bottom) {
+            if show {
+                Group {
+                    if #available(iOS 26, *) {
+                        CustomAlert(message: message, type: type)
+                            .transition(.asymmetric(insertion: .identity, removal: .opacity))
+                            .optionalGlassEffect(colorScheme, cornerRadius: 16)
+                    }
+                }
+                .offset(x: 0, y: -62)
+            }
         }
         .sensoryFeedback(.selection, trigger: selectedTab)
         

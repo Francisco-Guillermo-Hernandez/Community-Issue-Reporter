c92ced806f5133d467c8a7a452d728325aa4a544
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-03-28 23:35:34 -0600

    ft: rename file

--- Community Issue Reporter/Model/Coordinate.swift
+++ Community Issue Reporter/Model/Coordinate.swift
@@ -1,5 +1,5 @@
 //
-//  Location.swift
+//  Coordinate.swift
 //  Community Issue Reporter
 //
 //  Created by Francisco Hernandez on 28/3/26.
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Location {
+struct Coordinate: Codable {
     var latitude: Double
     var longitude: Double
 }

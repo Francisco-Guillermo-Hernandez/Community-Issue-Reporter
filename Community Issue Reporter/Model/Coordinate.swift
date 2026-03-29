5fef367744114524eb4ed49f345bce7fab073c7e
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-03-28 23:29:29 -0600

    ft: Bind the values with the parent component and load the last coordinated

---
+++ Community Issue Reporter/Model/Coordinate.swift
@@ -0,0 +1,13 @@
+//
+//  Location.swift
+//  Community Issue Reporter
+//
+//  Created by Francisco Hernandez on 28/3/26.
+//
+
+import Foundation
+
+struct Location {
+    var latitude: Double
+    var longitude: Double
+}

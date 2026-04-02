3af6eb9ece68a62f50793f7eadab146394cd05ff
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-04-02 16:09:14 -0600

    ft: Add a new property and init props

--- Community Issue Reporter/Model/Locator.swift
+++ Community Issue Reporter/Model/Locator.swift
@@ -9,7 +9,16 @@ import Foundation
 
 struct Locator: Identifiable, Codable {
     let id: String?
+    let countryCode: String
     let country: String
     let region: String
     let city: String
+    
+    init(id: String? = nil, countryCode: String, country: String, region: String, city: String) {
+        self.id = id
+        self.countryCode = countryCode
+        self.country = country
+        self.region = region
+        self.city = city
+    }
 }

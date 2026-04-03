1c256b3e68e3ff7a1f05a2e86f9decdebe06be7c
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-04-03 10:36:00 -0600

    ft: A new view was implemented to showcase what had been done by the user.

--- Community Issue Reporter/Views/Tabs/MyFindingsView.swift
+++ Community Issue Reporter/Views/Tabs/MyFindingsView.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct MyFindingsView: View {
+   
+    @State private var selectedOption: String = "My Reports"
+    
+    let options: [String] = ["My Reports", "My Petitions", "Signed Petitions"]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Picker("Options", selection: $selectedOption) {
+                    ForEach(options, id: \.self) { option in
+                        Text(option).tag(option)
+                    }
+                }
+                .pickerStyle(.segmented)
+                .padding(.top, 32)
+                
+                ScrollView(.vertical) {
+                    switch selectedOption {
+                    case "My Reports":
+                        MyReportsSubView(subViewName: selectedOption)
+                    case "My Petitions":
+                        MyPetitionsSubView(subViewName: selectedOption)
+                    case "Signed Petitions":
+                        SignedPetitionsSubView(subViewName: selectedOption)
+                    default:
+                        MyReportsSubView(subViewName: selectedOption)
+                    }
+                }
+            }
+            .navigationTitle("My Findings")
+            .padding(.horizontal, 16)
+        }
     }
 }
 

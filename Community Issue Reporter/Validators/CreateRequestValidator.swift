7965308d25f4d265f38410a9c975c72dfcede8f7
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-04-03 10:48:25 -0600

    ft: Add a new validator for Create Request view

--- Community Issue Reporter/Validators/CreateRequestValidator.swift
+++ Community Issue Reporter/Validators/CreateRequestValidator.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+let titleValidator: [Validator] = [
+    Validator(
+        name: "Isn't empty",
+        message: "The title can't be empty",
+        fn: { !$0.isEmpty }
+    ),
+    Validator(
+        name: "Isn't too long",
+        message: "The title can't be longer than 50 characters",
+        fn: { $0.count <= 50 }
+    ),
+    Validator(
+        name: "Isn't too short",
+        message: "The title can't be shorter than 5 characters",
+        fn: { $0.count >= 5 }
+    )
+]
+
+
+let descriptionValidator: [Validator] = [
+    Validator(
+        name: "Isn't empty",
+        message: "The description can't be empty",
+        fn: { !$0.isEmpty }
+    ),
+    Validator(
+        name: "Isn't too long",
+        message: "The description can't be longer than 50 characters",
+        fn: { $0.count <= 50 }
+    ),
+    Validator(
+        name: "Isn't too short",
+        message: "The description can't be shorter than 5 characters",
+        fn: { $0.count >= 5 }
+    )
+]

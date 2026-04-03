4c4b72f2ad62db5d4bbb0ee10bff4d6fee45260e
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-04-03 10:29:10 -0600

    ft: init values and some properties are now optional

--- Community Issue Reporter/Model/Petition.swift
+++ Community Issue Reporter/Model/Petition.swift
@@ -8,15 +8,29 @@
 import Foundation
 
 struct Petition: Identifiable, Codable {
-    let id: UUID
+    let id: String?
     let title: String
     let description: String
     let targetSignatures: Int
-    let currentSignatures: Int
+    let currentSignatures: Int?
     let categoryId: Int
-    let statusId: Int
-    let reportedBy: UUID
-    let disabled: Bool
-    let createdAt: Date
-    let updatedAt: Date
+    let statusId: Int?
+    let reportedBy: UUID?
+    let disabled: Bool?
+    let createdAt: Date?
+    let updatedAt: Date?
+    
+    init(id: String?, title: String, description: String, targetSignatures: Int, currentSignatures: Int?, categoryId: Int, statusId: Int?, reportedBy: UUID?, disabled: Bool?, createdAt: Date?, updatedAt: Date?) {
+        self.id = id
+        self.title = title
+        self.description = description
+        self.targetSignatures = targetSignatures
+        self.currentSignatures = currentSignatures
+        self.categoryId = categoryId
+        self.statusId = statusId
+        self.reportedBy = reportedBy
+        self.disabled = disabled
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+    }
 }

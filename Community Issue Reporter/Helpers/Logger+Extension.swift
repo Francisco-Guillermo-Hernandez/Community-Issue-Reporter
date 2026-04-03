ff6595eb67cef9309cb45dcd8a9a0297856ae8c5
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-04-03 10:43:30 -0600

    ft: Working on a helper to log info, warnings and errors

--- Community Issue Reporter/Helpers/Logger+Extension.swift
+++ Community Issue Reporter/Helpers/Logger+Extension.swift
@@ -5,4 +5,17 @@
 //  Created by Francisco Hernandez on 1/4/26.
 //
 
-import Foundation
+import OSLog
+
+extension Logger {
+    private static var subsystem = Bundle.main.bundleIdentifier!
+    static let viewCycle = Logger(subsystem: subsystem, category: "viewcycle")
+    static let statistics = Logger(subsystem: subsystem, category: "statistics")
+}
+
+
+class L {
+    func Notice() -> Void {
+        Logger.viewCycle.notice("")
+    }
+}

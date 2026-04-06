b0a3b885a50ac3b6bdd39fb0905f7ad1d0a202d6
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-04-05 21:52:32 -0600

    ft: Add licenses sub view

--- Community Issue Reporter/Views/SubViews/LicensesSubView.swift
+++ Community Issue Reporter/Views/SubViews/LicensesSubView.swift
@@ -8,13 +8,60 @@
 import SwiftUI
 
 struct LicensesSubView: View {
+    @Environment(\.dismiss) private var dismiss
     var subViewName: String
+
+    @State private var licenseText: String = ""
+
+    private func loadTextResource(
+        fileName: String,
+        fileExtension: String,
+        subdirectory: String? = nil
+    ) -> String {
+        guard let url = Bundle.main.url(
+            forResource: fileName,
+            withExtension: fileExtension,
+            subdirectory: subdirectory
+        ) else {
+            return "License text not available."
+        }
+
+        print("Loading: \(url.path)")
+        return (try? String(contentsOf: url, encoding: .utf8)) ?? "License text not available."
+    }
     var body: some View {
         NavigationStack {
-            Text("Hello")
+            ScrollView(.vertical, showsIndicators: true) {
+                LazyVStack {
+                    
+                    Text(self.licenseText)
+                    
+                    Divider()
+                }
+                .padding(20)
+                .task {
+                    self.licenseText =
+                        loadTextResource(
+                            fileName: "LICENSE_LITE",
+                            fileExtension: "TXT",
+                            subdirectory: ""
+                        )
+                    
+                }
+               
+            }
+            .navigationTitle(subViewName)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "arrow.down.backward")
+                    }
+                }
+            }
         }
-        .navigationTitle(subViewName)
-        .navigationBarTitleDisplayMode(.inline)
         .interactiveDismissDisabled(true)
     }
 }

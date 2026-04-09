4e8cb8eac74a9baf48e6e1de777c74e9f96388ba
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-04-09 00:01:40 -0600

    ft: Working on authentication

--- Community Issue Reporter/Model/AuthViewModel.swift
+++ Community Issue Reporter/Model/AuthViewModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+internal import Combine
+import GoogleSignIn
+
+class AuthViewModel: ObservableObject {
+    @Published var user: GIDGoogleUser?
+    @Published var isLoggedIn = false
+
+    func checkStatus() {
+        GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+            if let user = user {
+                self.user = user
+                self.isLoggedIn = true
+            }
+        }
+    }
+}

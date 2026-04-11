eabcbaa4ad82cd79fbccb2c6f34822a29a0df606
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-04-10 23:53:40 -0600

    ft: Add a new model

---
+++ Community Issue Reporter/Model/Requests.swift
@@ -0,0 +1,12 @@
+//
+//  Requests.swift
+//  Community Issue Reporter
+//
+//  Created by Francisco Hernandez on 9/4/26.
+//
+
+import Foundation
+
+struct OAuthSignInPayload: Encodable {
+    let token: String
+}

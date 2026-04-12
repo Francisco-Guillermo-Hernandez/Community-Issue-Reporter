0142dfa12c2235d8c7d9cfc8e257a1980689415a
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-04-12 00:11:32 -0600

    ft: working on a new component

---
+++ Community Issue Reporter/Components/CustomSegmentedControl.swift
@@ -0,0 +1,18 @@
+//
+//  CustomSegmentedControl.swift
+//  Community Issue Reporter
+//
+//  Created by Francisco Hernandez on 8/4/26.
+//
+
+import SwiftUI
+
+struct CustomSegmentedControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomSegmentedControl()
+}

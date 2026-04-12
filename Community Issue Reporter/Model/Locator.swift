ded9c13c2b71cf0c366074682e0301b669b0990a
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-04-12 00:04:49 -0600

    ft: modify models

--- Community Issue Reporter/Model/Locator.swift
+++ Community Issue Reporter/Model/Locator.swift
@@ -13,12 +13,25 @@ struct Locator: Identifiable, Codable {
     let country: String
     let region: String
     let city: String
+    let address: String
     
-    init(id: String? = nil, countryCode: String, country: String, region: String, city: String) {
+    init(id: String? = nil, countryCode: String, country: String, region: String, city: String, address: String = "") {
         self.id = id
         self.countryCode = countryCode
         self.country = country
         self.region = region
         self.city = city
+        self.address = address
+    }
+    
+    func withAddress(_ newAddress: String) -> Locator {
+        return Locator(
+            id: self.id,
+            countryCode: self.countryCode,
+            country: self.country,
+            region: self.region,
+            city: self.city,
+            address: newAddress
+        )
     }
 }
--- Community Issue Reporter/Model/Report.swift
+++ Community Issue Reporter/Model/Report.swift
@@ -11,6 +11,7 @@ struct Report: Identifiable, Codable {
     let id: String?
     let coordinate: Coordinate
     let address: String
+    let title: String
     let description: String
     let severityId: Int
     let statusId: Int
@@ -21,11 +22,13 @@ struct Report: Identifiable, Codable {
     let createdAt: Date?
     let updatedAt: Date?
     let reportedBy: String?
+    let olc: String?
     
     init(
             id: String? = nil,
             coordinate: Coordinate,
             address: String,
+            title: String,
             description: String,
             severityId: Int,
             statusId: Int,
@@ -33,13 +36,15 @@ struct Report: Identifiable, Codable {
             matterToSolveId: Int,
             reportedAt: Date? = nil,
             cellIndex: String,
+            olc: String? = nil,
             createdAt: Date? = nil,
             updatedAt: Date? = nil,
-            reportedBy: String? = nil
+            reportedBy: String? = nil,
         ) {
             self.id = id
             self.coordinate = coordinate
             self.address = address
+            self.title = title
             self.description = description
             self.severityId = severityId
             self.statusId = statusId
@@ -47,6 +52,7 @@ struct Report: Identifiable, Codable {
             self.matterToSolveId = matterToSolveId
             self.reportedAt = reportedAt
             self.cellIndex = cellIndex
+            self.olc = olc
             self.createdAt = createdAt
             self.updatedAt = updatedAt
             self.reportedBy = reportedBy

a49f3ee593499790980ffa9bf04a5a84484526a7
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-04-12 00:03:31 -0600

    ft: working on detail view

--- Community Issue Reporter/Components/DetailView.swift
+++ Community Issue Reporter/Components/DetailView.swift
@@ -6,17 +6,88 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 struct DetailView: View {
     @Environment(\.dismiss) private var dismiss
     var issue: IssueMarker
     var body: some View {
         NavigationStack {
-            VStack(alignment: .leading) {
-                Text(issue.title)
-                    .font(.subheadline)
-                    .bold()
+            VStack(alignment: .leading, spacing: 24) {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text(issue.title)
+                        .font(.title)
+                        .bold()
+                    
+                    Text(issue.description)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                }
+                
+                VStack {
+                    LazyHGrid(rows: gridColumns, spacing: 8) {
+//                        ForEach(1...4, id: \.self) { index in
+//                            
+//                            Button {
+//                                
+//                            } label: {
+//                                Text("1")
+//                                    .padding()
+//                                    .frame(maxWidth: .infinity)
+//                                
+//                                    .background(Color.secondary)
+//                                    .cornerRadius(8)
+//                                //                                           .foregroundColor(.secondary, )
+//                            }
+//                            .buttonStyle(.plain)
+//                            //                                   .buttonStyle(.glass)
+//                            .buttonSizing(.flexible)
+//                            .fixedSize(horizontal: true, vertical: true)
+//                        }
+                        
+                        VStack {
+                            Text("Issue Type")
+                                .font(.headline)
+                                .fontWeight(.black)
+                            
+                            Text(issue.issueType.title)
+                                .font(.footnote)
+                        }
+                        .padding()
+                        .background(Color.blue.secondary)
+                        .cornerRadius(16)
+                        
+                        VStack {
+                            Text("Severity")
+                                .font(.headline)
+                                .fontWeight(.black)
+                            
+                            Text(issue.severity.title)
+                                .font(.footnote)
+                        }
+                        .padding()
+                        .background(Color.blue.secondary.opacity(0.5))
+                        .cornerRadius(16)
+                        
+                        VStack {
+                            Text("Status")
+                                .font(.headline)
+                                .fontWeight(.black)
+                            
+                            Text(issue.status.title)
+                                .font(.footnote)
+                        }
+                        .padding()
+                        .background(Color.blue.secondary.opacity(0.5))
+                        .cornerRadius(16)
+                    }
+                    .fixedSize()
+                }
+                
+                
+                Spacer()
             }
+            .padding(.top, 16)
             .toolbar {
                 
                 ToolbarItem(placement: .cancellationAction) {
@@ -25,9 +96,9 @@ struct DetailView: View {
                     }
                 }
                 
-                ToolbarItem(placement: .title) {
-                    Text(issue.title)
-                }
+                //                ToolbarItem(placement: .title) {
+                //                    Text(issue.title)
+                //                }
                 
                 ToolbarSpacer(.fixed)
                 
@@ -42,8 +113,30 @@ struct DetailView: View {
         .presentationDetents([.medium, .fraction(0.2)])
         .presentationDragIndicator(.visible)
     }
+    
+    private  let gridColumns: [GridItem] = [
+        GridItem(.flexible(), spacing: 16),
+        
+    ]
 }
 
 #Preview {
-//    DetailView()
+    let coordinate = CLLocationCoordinate2D(
+        latitude: 37.7749,
+        longitude: -122.4194
+    )
+    
+    let issue = IssueMarker(
+        id: UUID().uuidString,
+        title: "A big pothole",
+        description: "There is a big pothole in the middle of the street",
+        status: 1,
+        coordinate: coordinate,
+        issueType: 1,
+        severity: 1,
+        matterToSolve: 1,
+        address: ""
+    )
+    
+    DetailView(issue: issue)
 }

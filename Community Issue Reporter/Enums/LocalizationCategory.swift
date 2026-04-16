a587cbbc15e87837b638a456a5e8409e38d8889b
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-04-15 23:47:13 -0600

    ft: Add new enum

---
+++ Community Issue Reporter/Enums/LocalizationCategory.swift
@@ -0,0 +1,16 @@
+//
+//  LocalizationCategory.swift
+//  Community Issue Reporter
+//
+//  Created by Francisco Hernandez on 15/4/26.
+//
+
+import Foundation
+
+enum LocationCategory: String {
+    case building
+    case publicSpace
+    case street
+    case restrictedOrPrivate
+    case unknown
+}

1071c88586c27a2ebbec6df24f21105f17c6b656
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-04-19 21:33:21 -0600

    ft: Add a new validator

---
+++ Community Issue Reporter/Validators/LocationValidator.swift
@@ -0,0 +1,13 @@
+//
+//  LocationValidator.swift
+//  Community Issue Reporter
+//
+//  Created by Francisco Hernandez on 15/4/26.
+//
+
+import Foundation
+import MapKit
+
+struct LocationValidator {
+    
+}

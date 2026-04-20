f0919e4056352ca48ec2186b16321fee2c8c85c6
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-04-19 21:28:11 -0600

    ft: add new request

--- Community Issue Reporter/Model/Responses.swift
+++ Community Issue Reporter/Model/Responses.swift
@@ -9,11 +9,46 @@ import Foundation
 
 
 struct GenericResponse: Identifiable, Codable {
-   var id: String
+    var id: String
+    var message: String
+    var code: String
+}
+
+struct UserTokens: Decodable {
+    let queryActionsToken: String
+    let mutationActionsToken: String
 }
 
 struct LoginWithOAuthProviderResponse: Decodable {
     var code: String
-    var authToken: String
+    var authToken: UserTokens
     var authProvider: String
+}
+
+struct PaginatedResponse<T: Decodable>: Decodable {
+    let documents: [T]?
+    let total: Int?
+    let page: Int?
+    let documentsPerPage: Int?
+    let totalPages: Int?
+    let hasNext: Bool
+    let hasPrev: Bool
+    
+    init(
+        documents: [T]? = [],
+        total: Int? = nil,
+        page: Int? = nil,
+        documentsPerPage: Int? = nil,
+        totalPages: Int? = nil,
+        hasNext: Bool = false,
+        hasPrev: Bool = false
+    ) {
+        self.documents = documents
+        self.total = total
+        self.page = page
+        self.documentsPerPage = documentsPerPage
+        self.totalPages = totalPages
+        self.hasNext = hasNext
+        self.hasPrev = hasPrev
+    }
 }

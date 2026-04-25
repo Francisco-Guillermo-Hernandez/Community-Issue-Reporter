17695fad25af0bc07fde17600276ea0e4dcae466
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-04-24 20:40:22 -0600

    ft: improve ux

--- Community Issue Reporter/Components/Badge.swift
+++ Community Issue Reporter/Components/Badge.swift
@@ -10,21 +10,42 @@ import SwiftUI
 struct Badge {
     let color: Color
     let title: String
+    let icon: String
 }
 
 struct CustomBadgeView: View {
     var badge: Badge
     var body: some View {
-        Capsule()
-            .fill(badge.color)
-            .glassEffect(in: .capsule)
-            .brightness(-0.2)
-            .frame(width: 85, height: 24)
-            .overlay {
-                Text(badge.title)
-                    .font(.footnote)
-                    .fontWeight(.medium)
-                    .foregroundStyle(.white)
-            }
+        
+        HStack {
+            Image(systemName: badge.icon)
+                .tint(.white)
+            Text(badge.title)
+                .font(.caption)
+                .fontWeight(.medium)
+        }
+        .frame(height: 24)
+        .foregroundStyle(Color.white)
+        .padding(.vertical, 4)
+        .padding(.horizontal)
+        .background(
+            Capsule()
+                .fill(badge.color)
+                .glassEffect(in: .capsule)
+                .brightness(-0.2)
+        )
+        .overlay {
+            Capsule()
+                .stroke(badge.color.opacity(0.7), lineWidth: 1)
+        }
+        
+    }
+}
+
+
+#Preview {
+    HStack {
+        CustomBadgeView(badge: Badge(color: .orange, title: "users", icon: "person.fill"))
+        CustomBadgeView(badge: Badge(color: .orange, title: "building", icon: "building"))
     }
 }

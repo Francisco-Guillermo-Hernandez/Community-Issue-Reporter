0bdc0946eb568a24ff70f4de4e7194209bc9f065
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-05-27 14:21:55 -0600

    ft: Add UserPersonalizationView

---
+++ Community Issue Reporter/Views/Tabs/UserPersonalizationView.swift
@@ -0,0 +1,24 @@
+//
+//  UserPersonalizationView.swift
+//  Community Issue Reporter
+//
+//  Created by Francisco Hernandez on 26/5/26.
+//
+
+import SwiftUI
+import WebKit
+
+struct UserPersonalizationView: View {
+    @State private var toggle = false
+
+
+    var body: some View {
+        VStack {
+           
+        }
+    }
+}
+
+#Preview {
+    UserPersonalizationView()
+}

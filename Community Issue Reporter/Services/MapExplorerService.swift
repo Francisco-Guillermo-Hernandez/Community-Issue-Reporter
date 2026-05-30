9a4b4338d4709c7a1d49aef6db37a840802981cb
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-05-30 10:00:13 -0600

    ft: Add a new service

---
+++ Community Issue Reporter/Services/MapExplorerService.swift
@@ -0,0 +1,21 @@
+//
+//  MapExplorerService.swift
+//  Community Issue Reporter
+//
+//  Created by Francisco Hernandez on 29/5/26.
+//
+
+import Foundation
+import SwiftMsgpack
+
+struct MapExplorerService {
+    
+    private let client: ServiceClient
+    init(client: ServiceClient =  ServiceClient(baseURL: development, decoderType: .messagePack(MsgPackDecoder()))) {
+        self.client = client
+    }
+    
+    func reports(q: MapExplorerQueryParams, h: [HTTPHeader]) async throws -> MapExplorerReports {
+        return try await client.get(path: "map-explorer/reports", query: q, headers: h, withOAuth: false)
+    }
+}

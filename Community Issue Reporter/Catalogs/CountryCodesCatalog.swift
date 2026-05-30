b6b0622fbe6b0fe22c4a2411950a10dc5b0187fa
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-05-30 09:51:37 -0600

    fix: update / add catalogs

---
+++ Community Issue Reporter/Catalogs/CountryCodesCatalog.swift
@@ -0,0 +1,24 @@
+//
+//  CountryCodesCatalog.swift
+//  Community Issue Reporter
+//
+//  Created by Francisco Hernandez on 29/5/26.
+//
+
+import Foundation
+
+enum CountryCode: String {
+    case SV = "SV"
+    case US = "US"
+    case GT = "GT"
+    case NI = "NI"
+    
+    var iso3166Alpha3Code: String {
+        switch self {
+            case .SV: return "SLV"
+            case .US: return "USA"
+            case .GT: return "GTM"
+            case .NI: return "NIC"
+        }
+    }
+}

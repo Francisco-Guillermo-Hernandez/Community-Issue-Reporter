0967e4fa2865064df2f2bd0fc91d8086864ab36d
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-06-04 00:02:50 -0600

    chore: move simple enums to a single file

---
+++ Community Issue Reporter/Enums/SimpleEnums.swift
@@ -0,0 +1,41 @@
+//
+//  SimpleEnums.swift
+//  Community Issue Reporter
+//
+//  Created by Francisco Hernandez on 3/6/26.
+//
+
+import Foundation
+
+// MARK: -
+enum ViewOptions: String {
+    case list
+    case listAndModify
+}
+
+// MARK: -
+enum LoginType {
+    case visitor
+    case user
+}
+
+enum TextBasedAvatarOptions: String {
+    case monogram
+    case initials
+}
+
+enum MonogramMode: String {
+    case preview
+    case send
+}
+
+// MARK: - 
+enum CurrentView {
+    case optionsSelector
+    case avatar
+    case camera
+    case photo
+    case initials
+    case monogram
+    case google
+}

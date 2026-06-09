b5ddd3975509487f04baff89eece30c4ae1285e0
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-06-09 11:56:26 -0600

    ft: add terms and conditions components

---
+++ Community Issue Reporter/Components/TermAndConditionsLinks.swift
@@ -0,0 +1,44 @@
+//
+//  TermAndConditionsLinks.swift
+//  Community Issue Reporter
+//
+//  Created by Francisco Hernandez on 7/6/26.
+//
+
+import SwiftUI
+
+struct LinksView: View {
+    var underlinedMarkdown: AttributedString {
+        let rawMarkdown = String(localized: "By continuing, you agree to our [Terms of Service](https://reportamelo.app/legal/terms) and [Privacy Policy](https://reportamelo.app/legal/privacy).")
+        
+        // Parse the raw Markdown string
+        guard var attributedString = try? AttributedString(markdown: rawMarkdown) else {
+            return AttributedString(rawMarkdown)
+        }
+        
+        // Loop through all segments to find links
+        for run in attributedString.runs {
+            if run.link != nil {
+                // Apply the underline style to the link range
+                attributedString[run.range].underlineStyle = .single
+            }
+        }
+        
+        return attributedString
+    }
+
+    var body: some View {
+        Text(underlinedMarkdown)
+            .font(.footnote)
+//            .font(.custom("Lora", size: 14, relativeTo: .footnote))
+            .foregroundColor(.secondary)
+            .tint(Color.theme.primary.mix(with: .white, by: 0.1))
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .kerning(-0.2)
+    }
+}
+
+
+#Preview {
+    LinksView()
+}

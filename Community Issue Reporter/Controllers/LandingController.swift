7123fca4f944fc8345c80dc400b86bf23a80c221
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-06-15 18:26:59 -0600

    feat: add controllers

---
+++ Community Issue Reporter/Controllers/LandingController.swift
@@ -0,0 +1,172 @@
+//
+//  LandingController.swift
+//  Community Issue Reporter
+//
+//  Created by Francisco Hernandez on 14/6/26.
+//
+
+import SwiftUI
+internal import Combine
+
+@MainActor
+class LandingController: ObservableObject {
+    @Published var presentAlert: Bool = false
+    @Published var message: String = ""
+    @Published var path: [LandingNavigation] = []
+    @Published var isGuest: Bool = false
+    @Published var isLoggedIn: Bool = false
+    @Published var isCheckingStatus: Bool = false
+    @Published var userOAuthState: UserOAuthResultState = .unowned
+    @Published var selectedCity: FriendlyCityDistribution
+    
+    @Published var countryCode: CountryCode = .SV
+    
+    private var settings: SettingsStore?
+    var profile: ProfileDataModel
+    init() {
+        self.profile = .init()
+        
+//        setReportLocatorSettings()
+        
+        selectedCity = .init(
+            cityId: "a67b90f9-1d76-4835-a994-03cd04f1d619",
+            firstLevel: "El Salvador",
+            secondLevel: "San Salvador",
+            thirdLevel: "San Salvador",
+            ZipCode: "1101",
+            legalGroupName: "Distrito de San Salvador",
+            coordinates: .init(lat: 13.701270, lng: -89.224432),
+            isCapitalCity: 1,
+            isDepartmentalCapital: 1
+        )
+    }
+    
+    func inject(_ settings: SettingsStore) {
+        self.settings = settings
+    }
+    
+    private func setReportLocatorSettings() -> Void {
+        
+        
+//        if !settings.countryCode.isEmpty {
+//            self.countryCode = settings.countryCodeIso
+//        }
+        
+//        if !settings.cityId.isEmpty {
+////            self.selectedCity.cityId = settings.cityId
+//        }
+        
+        
+    }
+    
+    func handleLogin(for session: String, with type: LoginType) {
+        if !session.isEmpty {
+            if type == .guest {
+                saveIntoKeychain(session)
+                self.isGuest = true
+            } else {
+                Task {
+        
+                    do {
+                        let (state, sessionId, data) = try await UserRepository.shared.login(session)
+                        
+                        self.userOAuthState = state
+                        /// Validates if the first time user login
+                        if state == .firstLogin {
+                            self.path.append(.selectCity)
+                        } else {
+                            
+                            if data.landingPageCompleted {
+                                /// Set preferences
+                                self.setSettingsFromAuthenticatedUser(with: data)
+                                self.isLoggedIn = true
+                            } else {
+                                
+                                /// Uncompleted landing process
+                                self.path.append(.selectCity)
+                            }
+                            
+                        }
+                        
+                        self.saveIntoKeychain(sessionId)
+                        
+                    } catch CommonIntercommunicationErrors.invalidPetition(let code) {
+                        self.showAlert(message: "CLNT: \(code)")
+                    } catch CommonIntercommunicationErrors.serverError(let code) {
+                        self.showAlert(message: String(localized: "SVR: \(code)"))
+                    } catch {
+                        self.showAlert(message: String(localized: "Something went wrong"))
+                    }
+                    
+                }
+            }
+        } else {
+            self.showAlert(message: String(localized: "Something went wrong"))
+        }
+    }
+    
+    private func saveIntoKeychain(_ sessionId: String) {
+        _ = KeychainService.save(key: .mutation, value: sessionId)
+    }
+    
+    private func showAlert(message: String) -> Void {
+        self.message = message
+        self.presentAlert = true
+    }
+    
+    private func setSettingsFromAuthenticatedUser(with data: PublicUserData) -> Void {
+        
+        /// Personalization settings
+        UserRepository.shared.setUsername(data.userName)
+        UserRepository.shared.setNames(data.names)
+        UserRepository.shared.setAvatar(url: data.profilePicture)
+        setAvatar(url: data.profilePicture, data.settings.avatarCreatedFrom)
+        
+        /// Notification settings
+        settings?.enableEmailNotifications = data.settings.notifications.email
+        settings?.enableWebNotifications = data.settings.notifications.web
+        
+        /// Privacy settings
+        settings?.showMyProfile = data.settings.privacySettings.showMyProfile
+        settings?.showMyUseNameWhenShare = data.settings.privacySettings.showMyUseNameWhenShare
+        
+        
+        /// Reporting settings
+        settings?.countryCode = data.settings.reportLocatorSettings.countryCode
+        settings?.cityId = data.settings.reportLocatorSettings.cityId
+        
+        
+        _ = KeychainService.save(key: .sessionStateVerification, value: "session:state:valid")
+    }
+    
+    private func setAvatar(url: String, _ createdFrom: AvatarCreatedFrom) -> Void {
+        self.profile.avatarURL = urlFromString(url)
+        self.profile.selectedAvatarOptionView = createdFrom
+    }
+    
+    func checkStatus() {
+        isLoggedIn = UserRepository.shared.isSessionValid()
+    }
+    
+    func logout() {
+        self.isLoggedIn = false
+        self.isGuest = false
+        
+        _ = KeychainService.deleteToken(key: .query)
+        _ = KeychainService.deleteToken(key: .mutation)
+        _ = KeychainService.deleteToken(key: .sessionStateVerification)
+        
+//            UserDefaults.standard.set(nil, forKey: "selected_city")
+        
+        let selectedOptionKey = "selected_avatar_option"
+        let selectedColorKey = "selected_avatar_color"
+        UserDefaults.standard.set(nil, forKey: selectedOptionKey)
+        UserDefaults.standard.set(nil, forKey: selectedColorKey)
+        UserDefaults.standard.set(nil, forKey: "map_latitude_delta")
+        UserDefaults.standard.set(nil, forKey: "map_longitude_delta")
+        UserDefaults.standard.set(nil, forKey: "avatar_url")
+        UserDefaults.standard.set(nil, forKey: "user_name")
+        UserDefaults.standard.set(nil, forKey: "selectedLanguageCode")
+    }
+    
+}
---
+++ Community Issue Reporter/Controllers/PetitionController.swift
@@ -0,0 +1,14 @@
+//
+//  PetitionController.swift
+//  Community Issue Reporter
+//
+//  Created by Francisco Hernandez on 14/6/26.
+//
+
+import SwiftUI
+internal import Combine
+
+@MainActor
+class PetitionController: ObservableObject {
+    
+}

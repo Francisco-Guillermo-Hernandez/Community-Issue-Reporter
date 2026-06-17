21b2e81bee0ff3db17a67011598e2af6d94f22b1
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-06-16 19:57:19 -0600

    ft: clean up

--- Community Issue Reporter/Components/StatsCardsView.swift
+++ Community Issue Reporter/Components/StatsCardsView.swift
@@ -15,35 +15,8 @@ struct StatsCardsView: View {
         VStack(spacing: .themeSpacing * 2) {
             HStack(spacing: .themeSpacing * 2) {
                 
-               
-//                Button {
-//                    path.append(.myReports)
-//                } label: {
-//                    CardInfoRow(
-//                        data: CardInfoModelView(
-//                            title: "Reports",
-//                            subtitle: "This month",
-//                            stat: "03"
-//                        ),
-//                        action: {}
-//                    )
-//                }
-//                .buttonStyle(.plain)
-//                .matchedTransitionSource(id: "transition:myReports", in: nameSpace) { configuration in
-//                    configuration
-//                        .background(Color.theme.primary)
-//                        .clipShape(RoundedRectangle(cornerRadius: .themeCardCornerRadius, style: .continuous))
-//                }
-                
                 NavigationLink(value: InsightsNavigation.myReports) {
-//                    CardInfoRow(
-//                        data: CardInfoModelView(
-//                            title: "Reports",
-//                            subtitle: "This month",
-//                            stat: "03"
-//                        ),
-//                        action: {}
-//                    )
+
                     StatCard(
                         action: {},
                         description: String(localized: "I've reported"),
@@ -52,22 +25,13 @@ struct StatsCardsView: View {
                         timeframe: ""
                     )
                 }
-//                .buttonStyle(.plain)
                 .matchedTransitionSource(id: "transition:myReports", in: nameSpace) { configuration in
                     configuration
-//                        .background(Color.theme.primary)
+                        .background(Color.theme.background)
                         .clipShape(RoundedRectangle(cornerRadius: .themeCardCornerRadius, style: .continuous))
                 }
                 
                 NavigationLink(value: InsightsNavigation.myPetitions) {
-//                    CardInfoRow(
-//                        data: CardInfoModelView(
-//                            title: "Petitions",
-//                            subtitle: "This month",
-//                            stat: "04"
-//                        ),
-//                        action: {}
-//                    )
                     StatCard(
                         action: {},
                         description: String(localized: "I've created"),
@@ -76,34 +40,11 @@ struct StatsCardsView: View {
                         timeframe: "for the last 6 months"
                     )
                 }
-//                .buttonStyle(.plain)
                 .matchedTransitionSource(id: "transition:myPetitions", in: nameSpace) { configuration in
                     configuration
-//                        .background(Color.theme.primary)
+                        .background(Color.theme.background)
                         .clipShape(RoundedRectangle(cornerRadius: .themeCardCornerRadius, style: .continuous))
                 }
-                
-                
-          
-                
-//                Button {
-//                    path.append(.myPetitions)
-//                } label: {
-//                    CardInfoRow(
-//                        data: CardInfoModelView(
-//                            title: "Petitions",
-//                            subtitle: "This month",
-//                            stat: "04"
-//                        ),
-//                        action: { }
-//                    )
-//                }
-//                .buttonStyle(.plain)
-//                .matchedTransitionSource(id: "transition:myPetitions", in: nameSpace) { configuration in
-//                    configuration
-//                        .background(Color.theme.primary)
-//                        .clipShape(RoundedRectangle(cornerRadius: .themeCardCornerRadius, style: .continuous))
-//                }
             }
             
             HStack {
@@ -118,7 +59,7 @@ struct StatsCardsView: View {
                 
                 StatCard(
                     action: {},
-                    description: String(localized: "I've commented on"),
+                    description: String(localized: "I've signed on"),
                     title: "100",
                     trend: String(localized: "Incidents"),
                     timeframe: "for the last 6 months"
@@ -139,7 +80,10 @@ struct StatsCardsView: View {
     @Previewable @Namespace var nameSpace
     @Previewable @State var path: [InsightsNavigation] = []
     NavigationStack(path: $path) {
-        StatsCardsView(path: $path, nameSpace: nameSpace)
+        ZStack {
+            Color.theme.background
+            StatsCardsView(path: $path, nameSpace: nameSpace)
+        }
     }
     .navigationDestination(for: InsightsNavigation.self) { destination in
         Text("Destination for \(String(describing: destination))")

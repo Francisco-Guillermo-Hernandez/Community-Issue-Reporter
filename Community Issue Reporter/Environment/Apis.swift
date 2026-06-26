597563febb99fd5786d22edb48217e816e1dbee8
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-06-25 22:34:53 -0600

    ft: Use xconfig

--- Community Issue Reporter/Environment/Apis.swift
+++ Community Issue Reporter/Environment/Apis.swift
@@ -7,7 +7,24 @@
 
 import Foundation
 
-let development = URL(string: "http://192.168.8.204:5050/")
-let authService = URL(string: "http://192.168.8.204:4099/api/v1/")
-let commentService = URL(string: "http://192.168.8.204:7879/api/v1/")
-let production = URL(string: "https://")
+let userAgent = "Reportamelo/1.0"
+
+enum Endpoints {
+    static var baseURL: URL {
+        guard let urlString = Bundle.main.object(forInfoDictionaryKey: "BASE_URL") as? String,
+              let url = URL(string: "https://\(urlString)") else {
+            fatalError("BASE_URL not found in Info.plist")
+        }
+        
+        return url
+    }
+    
+    static var shareableURL: URL {
+        guard let urlString = Bundle.main.object(forInfoDictionaryKey: "SHARE_URL") as? String,
+              let url = URL(string: "https://\(urlString)") else {
+            fatalError("SHARE_URL not found in Info.plist")
+        }
+        
+        return url
+    }
+}

30da0a135888d91887b55c17af4d9811618071aa
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-07-04 11:39:23 -0600

    ft: Update DeviceService

--- Community Issue Reporter/Services/DeviceService.swift
+++ Community Issue Reporter/Services/DeviceService.swift
@@ -11,26 +11,25 @@ import SwiftUI
 final class DeviceService {
     
     static let shared = DeviceService()
-    private init() {}
     
     func getDeviceId() -> String {
         
-        // Check if we already saved a device ID to the Keychain
+        /// Check if we already saved a device ID to the Keychain
         if let keychainDeviceID = KeychainService.loadToken(key: .deviceId) {
             return keychainDeviceID
         }
         
-        // Fallback: Get the Identifier for Vendor (IDFV)
+        /// Fallback: Get the Identifier for Vendor (IDFV)
         var newDeviceID = UIDevice.current.identifierForVendor?.uuidString
         
-        // Ultra-Fallback: If IDFV is somehow nil, generate a fresh UUID
+        /// Ultra-Fallback: If IDFV is somehow nil, generate a fresh UUID
         if newDeviceID == nil {
             newDeviceID = UUID().uuidString
         }
         
         let idToSave = newDeviceID!
         
-        // Save it to the Keychain so it survives app uninstalls
+        /// Save it to the Keychain so it survives app uninstalls
         _ = KeychainService.save(key: .deviceId, value: idToSave)
         
         return idToSave

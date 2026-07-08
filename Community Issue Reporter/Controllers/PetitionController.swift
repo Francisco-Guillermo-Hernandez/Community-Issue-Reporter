f19a7ccdd2dcb19ced3eef8eb3f337d2480e4625
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-07-08 13:45:49 -0600

    ft: Refactor viewModel to use observation instead

--- Community Issue Reporter/Controllers/PetitionController.swift
+++ Community Issue Reporter/Controllers/PetitionController.swift
@@ -2,15 +2,78 @@
 //  PetitionController.swift
 //  Community Issue Reporter
 //
-//  Created by Francisco Hernandez on 14/6/26.
+//  Created by Francisco Hernandez on 27/4/26.
 //
 
-import SwiftUI
-internal import Combine
+import Foundation
+import Observation
 
 @MainActor
-class PetitionController: ObservableObject {
+@Observable
+final class PetitionController {
     
-    @Published var petitions: [Petition] = []
-    @Published var isLoading: Bool = false
+    var reports: [Report]
+    var petition: Petition
+    var isTitleValid: Bool
+    var isSubmitting: Bool
+    var stepperAction: String
+    var isDescriptionValid: Bool
+    var minimumSignatures: Int
+   
+    init() {
+        self.reports = []
+        self.petition = .init(
+            id: "",
+            title: "",
+            description: "",
+            targetSignatures: 10,
+            currentSignatures: 0,
+            categoryId: 1,
+            statusId: 1,
+            reportedBy: UUID(),
+            disabled: false,
+            createdAt: Date(),
+            updatedAt: Date(),
+            reportsIds: [],
+            attachments: [],
+            postMetadata: .init(
+                audience: "", 
+                visibility: .draft,
+                countryCode: .SV,
+                language: "es",
+                shareLink: ""
+            ),
+            postPublisher: .init(
+                username: "", 
+                avatar: "",
+                profileId: ""
+            ),
+            reportsMetadata: []
+        )
+        
+        stepperAction = ""
+        isSubmitting = false
+        isTitleValid = false
+        isDescriptionValid = false
+        minimumSignatures = 10
+    }
+    
+    func prepareForModification(_ petition: Petition) {
+        self.petition = petition
+    }
+    
+    var isValid: Bool {
+        petition.title.isEmpty || petition.description.isEmpty || petition.targetSignatures == 0
+    }
+    
+    func fetchReports() async {
+        do {
+            let result = try await ReportRepository.shared.listByUser(page: 1)
+            guard let reports = result.documents else { return }
+            self.reports = reports
+            
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
 }

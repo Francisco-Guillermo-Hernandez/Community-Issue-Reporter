a964cd18d81ad068577ad771a36b4c0956d14434
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-07-20 22:42:56 -0600

    ft: add validators, bind id and focused state with textInput fields

--- Community Issue Reporter/Components/Wizard/DetailsView.swift
+++ Community Issue Reporter/Components/Wizard/DetailsView.swift
@@ -7,38 +7,87 @@
 
 import SwiftUI
 
+// MARK: - Definitions
+enum WizardElements: nonisolated Hashable, Sendable {
+    case reportTitle
+    case reportDescription
+    case reportAddress
+    
+    case shareButton
+    case copyCodeButton
+    
+    case final
+}
+
+// MARK: - View
 struct DetailsView: View {
     @Bindable var model: ReportDataModel
+    @FocusState.Binding var focusedField: WizardElements?
     
-    init(_ model: ReportDataModel) {
+    init(_ model: ReportDataModel, _ focusedField: FocusState<WizardElements?>.Binding) {
         self.model = model
+        self._focusedField = focusedField
     }
     
     var body: some View {
         VStack(alignment: .leading, spacing: 12) {
+            
+            /// Text field to give a tittle to the report
             TextInput(
-                name: "Title",
-                label: String(localized: "Title of the issue", comment: "ReportView: Title of the issue"),
-                isValid: .constant(true),
+                name: model.titlePlaceholder,
+                label: String(
+                    localized: "Title of the issue", 
+                    comment: "ReportView: Title of the issue"
+                ),
+                validators: titleValidator,
+                isValid: $model.isTitleValid,
                 value: $model.report.title,
             )
+            .id(WizardElements.reportTitle)
+            .focused($focusedField, equals: .reportTitle)
             
+            /// Text field to describe what occurred
             TextInput(
-                name: "Description",
-                label: String(localized: "Please describe the issue", comment: "ReportView: Please describe the issue"),
+                name: model.descriptionPlaceholder,
+                label: String(
+                    localized: "Please describe the issue", 
+                    comment: "ReportView: Please describe the issue"
+                ),
+                validators: descriptionValidator,
                 axis: .vertical,
-                isValid: .constant(true),
+                isValid: $model.isDescriptionValid,
                 value: $model.report.description,
-                
             )
+            .id(WizardElements.reportDescription)
+            .focused($focusedField, equals: .reportDescription)
+            
+            /// Text field to type address of the place where the problem occurred
+            TextInput(
+                name: "Address",
+                label: String(
+                    localized: "Please tell us where is the issue",
+                    comment: "ReportView: Please tell us where is the issue"
+                ),
+                validators: addressValidator,
+                regex: .customPattern(addressRegex),
+                axis: .vertical,
+                isValid: $model.isAddressValid,
+                value: $model.report.address,
+            )
+            .id(WizardElements.reportAddress)
+            .focused($focusedField, equals: .reportAddress)
+        }
+        .task {
+            focusedField = .reportTitle
         }
         .padding(.top, 4)
     }
 }
 
-
+// MARK: - Preview
 #Preview {
+    @Previewable @FocusState var focusedField: WizardElements?
     let model: ReportDataModel = ReportDataModel.shared
     model.setMatterToSolve(mattersToResolve.first!)
-    return DetailsView(model)
+    return DetailsView(model, $focusedField)
 }

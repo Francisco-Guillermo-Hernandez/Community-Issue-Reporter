173402b1de55a857f363a35e9139963d819b07ed
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-08-02 18:54:41 -0600

    ft: add city selector

--- Community Issue Reporter/WelcomeView.swift
+++ Community Issue Reporter/WelcomeView.swift
@@ -11,11 +11,36 @@ struct WelcomeView: View {
     @State private var controller = LandingController.shared
     @Environment(SettingsStore.self) var settings
     
+    @EnvironmentObject var appState: AuthViewModel
+    
     var body: some View {
         ZStack {
             
-            if controller.isLoggedIn || controller.isGuest {
+            if controller.isLoggedIn {
                 TabBarView()
+            } else if controller.isGuest {
+                if appState.selectedCity != nil {
+                    TabBarView()
+                } else {
+                    NavigationStack {
+                        CitySelectionView(
+                            countryCode: controller.countryCode,
+                            selectedCity: $controller.selectedCity,
+                            nextStep: {
+                                
+                                _ = KeychainService.save(key: .userType, value: UserType.guest.description)
+                                
+                                appState.selectedCity = controller.selectedCity
+                               
+                                appState.setCameraPosition(
+                                    to: controller.selectedCity.coordinates,
+                                    latitudeDelta:  0.005738743213994368,
+                                    longitudeDelta: 0.003718218254761041
+                                )
+                            }
+                        )
+                    }
+                }
             } else {
                 LandingView(controller: controller)
             }

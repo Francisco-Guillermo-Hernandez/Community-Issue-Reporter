39d0a785c2b7dc73278ef51b5b7ee96d5c858ce5
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2026-09-03 23:24:30 -0600

    ft: update

---
+++ Community Issue Reporter/Resources/Settings.swift
@@ -0,0 +1,14 @@
+//
+//  Settings.swift
+//  Community Issue Reporter
+//
+//  Created by Francisco Hernandez on 2/9/26.
+//
+
+import Foundation
+
+let MAX_REPORTS_PER_PAID_USERS = 150
+let MAX_REPORTS_PER_REGISTERED_USERS = 10
+let MAX_REPORTS_PER_UNREGISTERED_USERS = 1
+let MAX_REPORTS_PER_GUEST_USERS = 1
+let ALLOWED_COUNTRIES: [CountryCode] = [.SV]
